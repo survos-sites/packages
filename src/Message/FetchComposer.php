33d993f61fce783e35579f47cfc8ba7c617ae998
Tac Tacelosky <tacman@gmail.com>
2024-11-19 05:13:37 -0500

    refactor workflow some, add inspector

---
+++ src/Message/FetchComposer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Message;
+
+final class FetchComposer
+{
+    /*
+     * Add whatever properties and methods you need
+     * to hold the data for this message class.
+     */
+
+     public function __construct(
+         private readonly string $name,
+     ) {
+     }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+}
