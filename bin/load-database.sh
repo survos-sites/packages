b456b216ac3ce17b252f5a783f3bf68b8ea019ed
Tac Tacelosky <tacman@gmail.com>
2024-12-04 16:07:00 -0500

    remove fetch and process for now

--- bin/load-database.sh
+++ bin/load-database.sh
@@ -2,9 +2,9 @@ dbname=packages
 bin/console d:d:c --if-not-exists
 symfony console doctrine:migrations:migrate -n
 bin/console app:load-data -v --setup --limit 100000
-bin/console app:load-data -v --fetch
-bin/console app:load-data -v --fetch --process
-bin/console app:load-data -v  --process
+#bin/console app:load-data -v --fetch
+#bin/console app:load-data -v --fetch --process
+#bin/console app:load-data -v  --process
 
 bin/console d:query:sql "delete from package where marking='abandoned'"
 bin/console grid:index --reset
