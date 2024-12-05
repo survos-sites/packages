fa287d164d57541513e3a580b25f1efef0c1f9bb
Tac Tacelosky <tacman@gmail.com>
2024-12-05 06:44:01 -0500

    load sqlite

--- bin/load-database.sh
+++ bin/load-database.sh
@@ -1,6 +1,7 @@
 dbname=packages
-bin/console d:d:c --if-not-exists
-symfony console doctrine:migrations:migrate -n
+#bin/console d:d:c --if-not-exists
+#symfony console doctrine:migrations:migrate -n
+bin/console d:sch:update --force
 bin/console app:load-data -v --setup --limit 1000
 #bin/console app:load-data -v --fetch
 #bin/console app:load-data -v --fetch --process
