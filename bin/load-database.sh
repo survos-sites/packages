5be4d648d83f728fd58392ab2fa3d1f21706e291
Tac Tacelosky <tacman@gmail.com>
2024-12-20 07:37:33 -0500

    separate fetch and process

--- bin/load-database.sh
+++ bin/load-database.sh
@@ -2,9 +2,9 @@ dbname=packages
 bin/console d:d:c --if-not-exists
 symfony console doctrine:migrations:migrate -n
 bin/console app:load-data -v --setup --limit 1000
-#bin/console app:load-data -v --fetch
+bin/console app:load-data -v --fetch
 #bin/console app:load-data -v --fetch --process
-#bin/console app:load-data -v  --process
+./c :stats && bin/console app:load-data -v  --process && bin/console grid:index --reset
 
 bin/console dbal:run-sql "delete from package where marking='abandoned'"
 bin/console grid:index --reset
