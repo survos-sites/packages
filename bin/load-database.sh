0b5d62a5d6326be87ebc7ed943633a1e4ed4b953
Tac Tacelosky <tacman@gmail.com>
2025-05-17 06:15:39 -0400

    add easyadmin, tweak docs

--- bin/load-database.sh
+++ bin/load-database.sh
@@ -2,7 +2,7 @@ dbname=packages
 #bin/console d:d:c --if-not-exists
 #symfony console doctrine:migrations:migrate -n
 bin/console app:load-data
-bin/console workflow:migrate --marking=new --translation=load
+bin/console  workflow:iterate App\\Entity\\Package --marking=new --transition=load -v --limit 1
 
 bin/console dbal:run-sql "delete from package where marking='abandoned'"
 bin/console grid:index --reset
