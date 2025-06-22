d0949438227cb93884a1c8a12cc9cab424ab5859
Tac Tacelosky <tacman@gmail.com>
2025-06-22 06:13:01 -0400

    add m_Owner

--- src/Menu/AppMenu.php
+++ src/Menu/AppMenu.php
@@ -57,6 +57,7 @@ final class AppMenu implements KnpMenuHelperInterface
 //                     'kpa_Song',
                      'kpa_Video',
                      'm_px_victoria_obj',
+                     'm_Owner',
                      'dummy_products'] as $indexName) {
             $this->add($sub, 'app_insta', ['indexName' => $indexName], label: $indexName);
 
